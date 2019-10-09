5e91e8cbfa37bf0ccbf74deb34bfff854b8c612d
Thomas Culdaut <thomas@wildcodeschool.fr>
2019-10-09 17:26:36 +0200

    add commander button

--- src/Product.js
+++ src/Product.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import './Product.css'
-
 function Product (props) {
     const defaultImage = "https://www.tendance-miroir.com/734-large_default/miroir-point-d-interrogation-arial.jpg";
-    return <div className={props.available? "available" : "unavailable"}>
+    return <div style={{
+        backgroundColor: props.available ? "green" : "red"
+    }}>
         <h2>{props.name}</h2>
         <img alt="alt" src={props.image ? props.image : defaultImage} />
+        {props.available && <button>Commander</button>}
+        {props.available ? <h3>DISPO</h3> : <p>pas dispo</p>}
     </div>
 }
 
